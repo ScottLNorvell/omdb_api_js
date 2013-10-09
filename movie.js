5da625f8cee2474a3468997c39144bedb2045591
Scott Norvell <scottlnorvell@gmail.com>
2013-10-09 17:49:56 -0400

    finish mostly

--- movie.js
+++ movie.js
@@ -1,22 +1,77 @@
+
+
+
 function appendMovieInfo(data){
-  console.log(data);
-  //append the movie Info below the clicked movie
+  
+  var li = $("#" + data.imdbID);
+
+  var h2 = $('<h2>');
+  var h3 = $('<h3>');
+  var p = $('<blockquote>');
+  var img = $('<img>')
+
+  li.addClass("movie")
+
+  h2.text(data.Title);
+  h3.text(data.Year);
+  p.text(data.Plot);
+  img.attr('src', data.Poster);
+
+  li.empty();
+
+  li.append(h2).append(h3).append(img).append(p);
+
+  // results.append(li);
+
 }
 
 function appendMovies(data){
-  console.log(data);
-  //append the movies to the DOM
+  // console.log(data);
+  results.empty();
+  var mov_list = data.Search;
+  md = mov_list;
+  $.each(mov_list, function(i, movie) {
+    var li = $('<li>');
+    var a = $('<a>');
+    var h3 = $('<h3>');
+
+    li.addClass("movie")
+    li.attr('id', movie.imdbID)
+
+    a.text(movie.Title).attr('href','#');
+    a.on('click', function() {
+      var self = $(this);
+
+      var title = self.text();
+      $.getJSON(base_url, {t: title}, appendMovieInfo)
+
+    });
+    h3.append(a);
+    li.append(h3);
+    results.append(li);
+
+  });
 }
 
+var results;
+var base_url = 'http://www.omdbapi.com/'
+
 $(function(){
-  $('form').submit(function(){
-    var request_url = 'http://www.omdbapi.com/';
-    $.getJSON(request_url)({
-      //set up the ajax call to the above URL
-    })
-    .done(appendMovies);
+
+  results = $('#results');
+  var search_form = $('#search-form')
+
+  search_form.submit(function(e){
+    e.preventDefault()
+    var self = $(this)
+    var request_url = base_url + "?" + self.serialize();
+    $.getJSON(request_url).done(appendMovies);
     return false;
   });
+
+  $('#search-button').on('click', function() {
+    search_form.submit();
+  })
 
 
 });
